76cc27ba3ac833b13a304bf3286d286a374c0e6d
Luciano Crocco <lucianoacrocco@gmail.com>
2022-03-08 20:40:14 -0300

    Comienzo HTTP request

---
+++ 4. Peticiones HTTP/1. Objeto XMLHTTPRequest/script.js
@@ -0,0 +1,30 @@
+/*
+Protocolo HTTP: https://es.wikipedia.org/wiki/Protocolo_de_transferencia_de_hipertexto
+Códigos de respuesta HTTP: https://developer.mozilla.org/es/docs/Web/HTTP/Status
+*/
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+    let xhr; 
+    if(window.XMLHttpRequest){
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+    
+    xhr.addEventListener("load", (data) => { // Con load sabemos que la petición a terminado. Despues indicamos en el metodo anonimo que hacer con esa información.
+        const dataJSON = JSON.parse(data.target.response);
+        
+        const list = document.getElementById("list");
+        for(let userInfo of dataJSON){
+            const listItem = document.createElement("li");
+            listItem.textContent = `${userInfo.id} - ${userInfo.name}`
+            list.appendChild(listItem);
+        }
+
+    }); 
+
+});
